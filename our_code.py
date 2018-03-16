c355da55ff0190c42b11214947ce73cd3e3af0d8
Jake <kartchner56jake@gmail.com>
2018-03-16 10:54:21 -0600

    updated features

--- our_code.py
+++ our_code.py
@@ -1,4 +1,10 @@
 print("This is our awesome python program code")
 
-def crazy
+def crazy();
+	print("Hello Worlds")
+
+def feature_component();
+	print("Hello Worlds")
+
+def frame_component();
 	print("Hello Worlds")
