20b1b5ff48a8255a6df9b0e49f8a1f8e0099b0bd
JKartchner24 <36463882+JKartchner24@users.noreply.github.com>
2018-03-16 11:11:46 -0600

    Update our_code.py

--- our_code.py
+++ our_code.py
@@ -1,6 +1,5 @@
 print("This is our awesome python program code")
 
-<<<<<<< HEAD
 def crazy();
 	print("Hello Worlds")
 
@@ -9,10 +8,8 @@ def feature_component();
 
 def frame_component();
 	print("Hello Worlds")
-=======
 def crazy():
 	print("Hello Worlds")
 
 def hello():
 	print("This is my test file")
->>>>>>> second_feature
